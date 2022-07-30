3af7a2b4c8de406c9b48b9854bc367ca5f7522ce
denis.menacai <denis.menacai@net.vision>
2022-07-30 13:56:16 +0300

    afiseaza erori la produse edit si oferta

--- src/app/nomenclatoare/produse/produse-item/produse-edit/produse-edit.component.ts
+++ src/app/nomenclatoare/produse/produse-item/produse-edit/produse-edit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { furnizoriDTO } from 'src/app/nomenclatoare/furnizori/furnizori-item/furnizori.model';
 import { FurnizoriService } from 'src/app/nomenclatoare/furnizori/furnizori.service';
+import { parseWebAPIErrors } from 'src/app/utilities/utils';
 import { ProduseService } from '../../produse.service';
 import { produseCreationDTO,produseDTO } from '../produse.model';
 
@@ -15,6 +16,7 @@ export class ProduseEditComponent implements OnInit {
   constructor(private activatedRoute: ActivatedRoute,private router:Router, private produsService: ProduseService,
       private furnizorService: FurnizoriService) { }
   
+  errors: string[] = [];
   preselectFurnizor: furnizoriDTO | undefined;
   model!:produseDTO;
   ngOnInit(): void {
@@ -35,7 +37,8 @@ export class ProduseEditComponent implements OnInit {
     this.produsService.edit(this.model.id, produseCreationDTO)
     .subscribe(() => {
       this.router.navigate(["/produse"]);
-    });
+    }, 
+    error=> this.errors = parseWebAPIErrors(error));    
   }
 
 }
