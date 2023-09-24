616a71748f27485c77f95bc03abb683b79869e64
denismena <denis.menacai@gmail.com>
2023-09-24 18:07:04 +0300

    after material migration script

--- src/app/comenzi/comenzi-furn-select-dialog/comenzi-furn-select-dialog.component.ts
+++ src/app/comenzi/comenzi-furn-select-dialog/comenzi-furn-select-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+//import { MatLegacyDialogRef as MatDialogRef, MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA } from '@angular/material/legacy-dialog';
 import { RxwebValidators } from '@rxweb/reactive-form-validators';
 import { comenziFurnizorBasicDTO } from 'src/app/comenzi-furn/comenzi-furn-item/comenzi-furn.model';
 import { ComenziFurnizorService } from 'src/app/comenzi-furn/comenzi-furn.service';
@@ -31,12 +32,12 @@ export class ComenziFurnSelectDialogComponent implements OnInit {
     });
   }
 
-  submit(form: NgForm) {    
-    console.log('form:', form);
+  submit(form: NgForm) {        
     this.dialogRef.close({
       clicked: 'submit',
       form: form      
     });
   }
+
 
 }
